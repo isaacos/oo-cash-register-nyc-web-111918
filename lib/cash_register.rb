e12c85c291f4dce70dd3613587dbcfb9a4fc76d6
Isaac Oseas <oseasisaac91@gmail.com>
2018-11-21 14:47:51 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,7 +16,7 @@ class CashRegister
   def add_item (title, price, quantity =1)
     @total += price * quantity
     @items.concat ([title] * quantity)
-    self.transaction.push(price * quantity)
+    @transaction.push(price * quantity)
     
   end
   
